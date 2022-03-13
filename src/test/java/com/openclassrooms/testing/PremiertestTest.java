7895e16c556b85deaa484a78ea163156f4f376dc
OlivierMOREL <oliviermorel.oc1@gmail.com>
2022-03-13 22:16:21 +0100

    feat : first test codded en succes

---
+++ src/test/java/com/openclassrooms/testing/PremiertestTest.java
@@ -0,0 +1,23 @@
+package com.openclassrooms.testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PremiertestTest {
+
+	@Test
+	void testAddTwoPositiveNumbers() {
+		// Arrange
+		int a = 2, b = 3;
+		Premiertest premiertest = new Premiertest();
+
+		// Act
+		int somme = premiertest.add(a, b);
+
+		// Assert
+		assertEquals(5, somme);
+
+	}
+
+}
